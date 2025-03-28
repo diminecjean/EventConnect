069cd2ba5f99aecbca0de256efcbd5e79e1fb9ff
diminecjean <looi.weien02@student.usm.my>
2025-03-28 11:21:25 +0800

    task(api): add org api route

---
+++ app/api/organizations/route.tsx
@@ -0,0 +1,49 @@
+import { connectToDB } from "../../lib/mongodb";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export const GET = async (_req: NextRequest) => {
+  try {
+    const client = await connectToDB();
+    const db = client.db();
+
+    const organizations = await db.collection("organizations").find({}).toArray();
+
+    return NextResponse.json({ organizations }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching organizations:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch organizations" },
+      { status: 500 },
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const client = await connectToDB();
+    const db = client.db();
+    
+    const body = await req.json();
+    
+    if (!body || !body.name || !body.email) {
+      return NextResponse.json(
+        { error: "Missing required user fields" },
+        { status: 400 }
+      );
+    }
+    
+    const result = await db.collection("organizations").insertOne(body);
+    
+    return NextResponse.json(
+      { message: "User added successfully", userId: result.insertedId },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error adding user:", error);
+    return NextResponse.json(
+      { error: "Failed to add user" },
+      { status: 500 }
+    );
+  }
+};
