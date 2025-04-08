0869a51a20655c7ec27025ecff31f88cb9bf015a
diminecjean <looi.weien02@student.usm.my>
2025-04-08 17:44:45 +0800

    chore: add loader
    - add framer-motion package

--- app/events/[id]/loading.tsx
+++ app/events/[id]/loading.tsx
@@ -1,4 +1,28 @@
-export default function Loading() {
+import { MultiStepLoader as Loader } from "@/components/ui/multi-step-loader";
+
+const loadingStates = [
+  {
+    text: "Getting Events",
+  },
+  {
+    text: "Finding Speakers",
+  },
+  {
+    text: "Gathering Attendees",
+  },
+];
+
+export default function MultiStepLoaderDemo() {
+  return (
+    <div className="w-full h-[60vh] flex items-center justify-center">
+      {/* Core Loader Modal */}
+      {/* <Loader loadingStates={loadingStates} loading={true} duration={2000} /> */}
+      Loading Event ...
+    </div>
+  );
+}
+
+export function Loading() {
   return (
     <div className="p-6 max-w-2xl mx-auto animate-pulse">
       <div className="h-8 bg-gray-300 rounded w-3/4 mb-4"></div>
