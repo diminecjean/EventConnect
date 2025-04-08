3a9b028e4ad76b7f0012e3ffdf3bd91a0d6dca84
diminecjean <looi.weien02@student.usm.my>
2025-04-08 17:35:12 +0800

    chore: add more image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["assets.aceternity.com", "cdn.prod.website-files.com", "res.cloudinary.com"],
+        domains: ["assets.aceternity.com", "cdn.prod.website-files.com", "res.cloudinary.com", "avatars.githubusercontent.com"],
   }
 }
 
